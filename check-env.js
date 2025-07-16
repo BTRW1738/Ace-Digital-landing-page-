c54ff37aca2155907ba94f89fd56207d4f920000
BTRW1738 <Bkltwork22@gmail.com>
2025-07-16 18:25:10 +0100

    Deleted check-env.js

--- check-env.js
+++
@@ -1,5 +0,0 @@
-// Run this in your browser console on your website
-console.log('Environment Variables Check:');
-console.log('VITE_SUPABASE_URL:', import.meta.env.VITE_SUPABASE_URL);
-console.log('VITE_SUPABASE_ANON_KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY);
-console.log('All env vars:', import.meta.env);
